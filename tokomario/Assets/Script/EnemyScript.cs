d7699c4d6b543613c35c9299304e748f04625c12
k-n-gk <gotsupuri1007@gmail.com>
2016-10-17 18:48:53 +0900

    キャラ移動

---
+++ tokomario/Assets/Script/EnemyScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : Token {
+
+	// Use this for initialization
+	void Start () {
+        SetSize(SpriteWidth / 2, SpriteHeight / 2);
+
+        float dir = Random.Range(0, 359);
+
+        float spd = 2;
+
+        SetVelocity(dir, spd);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 min = GetWorldMin();
+
+        Vector2 max = GetWorldMax();
+
+        if(X < min.x || max.x < X)
+        {
+            VX *= -1;
+            ClampScreen();
+        }
+        if (Y < min.y || max.y < Y)
+        {
+            VY *= -1;
+            ClampScreen();
+        }
+        
+    }
+    public void OnMouseDown()
+    {
+        DestroyObj();
+    }
+}
---
+++ tokomario/Assets/Script/Tokomove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tokomove : MonoBehaviour {
+    public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("right"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+	}
+}
