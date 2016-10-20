e469407295cee3d9c25a2f3403737e463c69085d
k-n-gk <gotsupuri1007@gmail.com>
2016-10-20 19:40:04 +0900

    コイン取得数UI表示

---
+++ tokomario/Assets/PlayerState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerState : MonoBehaviour {
+    int CoinCount;
+
+    public Text Cointext;
+	// Use this for initialization
+	void Start () {
+        CoinCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Cointext.text = string.Format("x{0:00}", CoinCount);
+	}
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == ("Coin"))
+            CoinCount++;
+    }
+}
